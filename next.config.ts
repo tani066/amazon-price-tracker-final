50a0947852f598089f536a24dc1c041383d1264d
Tanishk Agrawal065 <tanishkagrawalprsnl@Tanishks-MacBook-Pro.local>
2025-05-28 00:27:14 +0530

    change dependency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' https://ssl.gstatic.com https://accounts.google.com 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https://images-na.ssl-images-amazon.com https://m.media-amazon.com;
+              connect-src 'self';
+              font-src 'self';
+            `.replace(/\n/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
